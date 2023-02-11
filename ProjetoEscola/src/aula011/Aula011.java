0eb4ebe2b6fbbb0968b8d9900be4b155fbb26c62
Murilo Schneider Santos <113919662+MuriloSchneiderS@users.noreply.github.com>
2023-02-11 15:24:57 -0300

    ProjetoEscola adicionado

---
+++ ProjetoEscola/src/aula011/Aula011.java
@@ -0,0 +1,9 @@
+package aula011;
+
+public class Aula011 {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
---
+++ ProjetoEscola/src/aula011/alunoTecnico.java
@@ -0,0 +1,17 @@
+package aula011;
+
+public class alunoTecnico extends Aluno{
+    private String registroProfissional;
+    //
+    public void praticar(){
+        System.out.println("Praticando...");
+    }
+    //
+    public String getRegistroProfissional(){
+        return registroProfissional;
+    }
+    
+    public void setRegistroProfissional(String valor){
+        this.registroProfissional=valor;
+    }
+}
